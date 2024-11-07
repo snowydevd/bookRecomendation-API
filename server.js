72e49d4943bbf10c481b8ef1955610f9ea71ae25
snowydevd <lautarochini4@gmail.com>
2024-11-07 20:42:58 -0300

    search another port if already in use function added

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 const express = require("express");
+const net = require("net");
 const PORT = 3000;
 
 const app = express();
-
 app.use(express.json());
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log("No se ha podido conectar a este puerto");
-  }
-  console.log("\n#########################\n");
-  console.log(
-    "\x1b[36m%s\x1b[0m",
-    `Escuchando al server (http://localhost:${PORT})`
-  );
-});
+function checkPort(port, callback) {
+  const server = net.createServer();
+
+  server.once("error", (err) => {
+    if (err.code === "EADDRINUSE") {
+      callback(false);
+    } else {
+      callback(err);
+    }
+  });
+
+  server.once("listening", () => {
+    server.close();
+    callback(true);
+  });
+
+  server.listen(port);
+}
+
+function startServer(port) {
+  checkPort(port, (isAvailable) => {
+    if (isAvailable) {
+      app.listen(port, () => {
+        console.log(
+          "\x1b[36m%s\x1b[0m",
+          `Escuchando al server (http://localhost:${PORT})`
+        );
+      });
+    } else {
+      console.log(`${port} is already in use, changing port to: ${port + 1}`);
+      startServer(port + 1);
+    }
+  });
+}
+
+startServer(PORT);
